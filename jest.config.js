135066873b0215972cee56090d4a8bb8261a6b40
fredafei <964959033@qq.com>
2020-03-06 13:12:50 +0800

    add jest alias concat webpack alias

--- jest.config.js
+++ jest.config.js
@@ -2,9 +2,9 @@ module.exports = {
   verbose: true,
   clearMocks: false,
   collectCoverage: false,
-  reporters: ['default','jest-junit'],
+  reporters: ['default', 'jest-junit'],
   globals: {
-    'ts-jest':{
+    'ts-jest': {
       tsConfig: 'tsconfig.test.json',
     }
   },
@@ -13,7 +13,11 @@ module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/test/__mocks__/fileMock.js',
-    '\\.(css|less|scss)$': '<rootDir>/test/__mocks__/styleMock.js'
+    '\\.(css|less|scss)$': '<rootDir>/test/__mocks__/styleMock.js',
+    // webpack alias -> jest alias
+    '^views(.*)$': '<rootDir>/src/views$1',
+    '^utils(.*)$': '<rootDir>/src/utils$1',
+    '^components(.*)$': '<rootDir>/src/components$1',
   },
   testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
   transform: {
@@ -21,4 +25,4 @@ module.exports = {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
   setupFilesAfterEnv: ['<rootDir>test/setupTests.js']
-}
+};
