ed358f96c28d100bdb976bb4e8e3200918c0129b
fredafei <964959033@qq.com>
2020-05-13 13:37:39 +0800

    update http

--- src/utils/axiosMiddleware.ts
+++ src/utils/axiosMiddleware.ts
@@ -7,14 +7,15 @@ const baseParams = {
   ts: Math.floor(Date.now() / 1000)
 }
 
-function qs(data:any) {
+function qs(data: any) {
   let ret = ''
-  const resultData = Object.assign({...baseParams}, data || {})
+  const resultData = Object.assign({ ...baseParams }, data || {})
   for (let it in resultData) {
     ret += `${encodeURIComponent(it)}=${encodeURIComponent(resultData[it])}&`
   }
   return ret
 }
+
 //https://test.xx.cn
 //https://m.xx.com
 const axiosMiddleware = axios.create({
@@ -22,34 +23,34 @@ const axiosMiddleware = axios.create({
   timeout: 10000
 })
 axiosMiddleware.interceptors.request.use(
-  (config:AxiosRequestConfig) => {
+  (config: AxiosRequestConfig = {}) => {
     if (config.method === 'get') {
-      config.params = Object.assign({...baseParams}, config.params || {})
+      config.params = Object.assign({ ...baseParams }, config.params || {})
     }
     if (config.method === 'post') {
       config.transformRequest = [qs]
     }
     return config
   },
-  (error:AxiosError) => {
+  (error: AxiosError) => {
     return Promise.reject(error)
   }
 )
 
 axiosMiddleware.interceptors.response.use(
-  (res:AxiosResponse) => {
-    const res_code = +res.data.res_code
+  (res: AxiosResponse) => {
+    const { code } = res.data
     //登录失效code则返回登录页
-    if (res_code === 999) {
+    if (code === 999) {
       return Promise.reject(res)
     }
     //请求失败
-    if (res_code === 0) {
+    if (code === 0) {
       return Promise.reject(res)
     }
-    return res
+    return Promise.resolve(res)
   },
-  (error:AxiosError) => {
+  (error: AxiosError) => {
     return Promise.reject(error)
   }
 )
--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,18 +1,23 @@
-import { AxiosError, AxiosResponse } from 'axios';
+import { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import axiosMiddleware from './axiosMiddleware'
 import utils from './help'
 
 export function addTokenAndKeyParams(options = {}) {
-  return Object.assign({token: utils.getStorage('localStorage', 'token') || ''}, options)
+  return Object.assign({ token: utils.getStorage('localStorage', 'token') || '' }, options)
 }
-const http = (options:any)  => {
-  let {url, method = 'get', ...rest} = options
+
+interface apIResponse<T = any> {
+  message: string
+  status: number
+  result: T
+}
+
+const http = (options: AxiosRequestConfig = {}): Promise<apIResponse<T>> => {
+  let { url, method = 'get', ...rest } = options
   return new Promise((resolve, reject) => {
-    axiosMiddleware({url, method, ...rest}).then(
-      (res:AxiosResponse) => {
-        resolve(res.data)
-      },
-      (err:AxiosError) => reject(err)
+    axiosMiddleware({ url, method, ...rest }).then(
+      (res: AxiosResponse) => resolve(res.data),
+      (err: AxiosError) => reject(err)
     )
   })
 }
